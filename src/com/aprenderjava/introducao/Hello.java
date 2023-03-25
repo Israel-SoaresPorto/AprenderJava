0f997939190b499916686bdea30c97051693de9d
Israel-SoaresPorto <110688515+Israel-SoaresPorto@users.noreply.github.com>
2023-03-25 17:58:22 -0300

    primeiro programa

---
+++ src/com/aprenderjava/introducao/Hello.java
@@ -0,0 +1,11 @@
+package com.aprenderjava.introducao;
+
+/**
+ * Mensagen de olá mundo
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
